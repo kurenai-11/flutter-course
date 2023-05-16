bbe9cb5d976bbf3c590fa72857c0035620c29908
kurenai-11 <moldrenkailich@gmail.com>
2023-05-17 02:52:44 +0300

    improved

--- f4/lib/question.dart
+++ f4/lib/question.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
-class Question extends StatelessWidget {
-  final String category;
+class QuestionData {
+  final QuestionCategory category;
   final String questionContent;
+
+  const QuestionData({required this.category, required this.questionContent});
+}
+
+enum QuestionCategory {
+  math(
+      imageSrc:
+          "https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000"),
+  science(
+      imageSrc:
+          "https://img.freepik.com/free-vector/colourful-science-work-concept_23-2148539571.jpg?w=2000"),
+  history(
+      imageSrc:
+          "https://s7d2.scene7.com/is/image/Kennametal/History?\$MobileBanner\$&");
+
+  const QuestionCategory({required this.imageSrc});
+
   final String imageSrc;
+
+  @override
+  String toString() {
+    final nameArray = name.split("");
+    return "${nameArray[0].toUpperCase()}${nameArray.sublist(1).join()}";
+  }
+}
+
+class Question extends StatelessWidget {
+  final QuestionData question;
 
   const Question({
     super.key,
-    required this.category,
-    required this.questionContent,
-    required this.imageSrc,
+    required this.question,
   });
 
   @override
@@ -18,14 +43,14 @@ class Question extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         Text(
-          category,
+          question.category.toString(),
           style: const TextStyle(fontSize: 48),
         ),
         const SizedBox(
           height: 12,
         ),
         Image.network(
-          imageSrc,
+          question.category.imageSrc,
           width: 200,
           height: 150,
           fit: BoxFit.cover,
@@ -34,7 +59,7 @@ class Question extends StatelessWidget {
           height: 12,
         ),
         Text(
-          questionContent,
+          question.questionContent,
           style: const TextStyle(fontSize: 24),
           textAlign: TextAlign.center,
         )
