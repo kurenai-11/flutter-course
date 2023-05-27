25e9d06bf1dc1017b2395e776d751ec777e14530
kurenai-11 <moldrenkailich@gmail.com>
2023-05-27 10:25:33 +0300

    changes

--- f5/lib/main.dart
+++ f5/lib/main.dart
@@ -8,6 +8,41 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  final List<QuestionData> questionsData = const [
+    QuestionData(
+        category: QuestionCategory.math, questionContent: "What is 2+2?"),
+    QuestionData(
+        category: QuestionCategory.math, questionContent: "What is 5*10?"),
+    QuestionData(
+        category: QuestionCategory.math, questionContent: "What is 11*32?"),
+    QuestionData(
+        category: QuestionCategory.math,
+        questionContent: "What is the factorial of 11?"),
+    QuestionData(
+        category: QuestionCategory.math,
+        questionContent: "What is the limit of x^2 - 4?"),
+    QuestionData(
+        category: QuestionCategory.math,
+        questionContent: "What is the shape of a circle?"),
+    QuestionData(
+        category: QuestionCategory.math,
+        questionContent:
+            "How many degrees does a triangle have total for every corner?"),
+    QuestionData(
+        category: QuestionCategory.science,
+        questionContent: "How do you define a quantum singularity?"),
+    QuestionData(
+        category: QuestionCategory.science,
+        questionContent:
+            "What is the best angle to throw a ball for it to have the largest travel distance?"),
+    QuestionData(
+        category: QuestionCategory.history,
+        questionContent: "What is the meaning of life?"),
+  ];
+
+  Iterable<Question> get questions =>
+      questionsData.map((q) => Question(question: q));
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -23,12 +58,12 @@ class MyApp extends StatelessWidget {
           child: SingleChildScrollView(
             child: Container(
               padding: const EdgeInsets.all(6),
-              child: const Column(
+              child: Column(
                 children: [
-                  SizedBox(
+                  const SizedBox(
                     height: 12,
                   ),
-                  Text(
+                  const Text(
                     "I am too lazy to search for different images...",
                     textAlign: TextAlign.center,
                     style: TextStyle(
@@ -49,71 +84,10 @@ class MyApp extends StatelessWidget {
                       fontSize: 24,
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 12,
                   ),
-                  Question(
-                    category: 'Math',
-                    questionContent: 'What is 2+2?',
-                    imageSrc:
-                        'https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000',
-                  ),
-                  Question(
-                    category: 'Math',
-                    questionContent: 'What is 5*10?',
-                    imageSrc:
-                        'https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000',
-                  ),
-                  Question(
-                    category: 'Math',
-                    questionContent: 'What is 11*32?',
-                    imageSrc:
-                        'https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000',
-                  ),
-                  Question(
-                    category: 'Math',
-                    questionContent: 'What is the factorial of 11?',
-                    imageSrc:
-                        'https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000',
-                  ),
-                  Question(
-                    category: 'Math',
-                    questionContent: 'What is the limit of x^2 - 4?',
-                    imageSrc:
-                        'https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000',
-                  ),
-                  Question(
-                    category: 'Math',
-                    questionContent: 'What is the shape of the cirlcle?',
-                    imageSrc:
-                        'https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000',
-                  ),
-                  Question(
-                    category: 'Math',
-                    questionContent:
-                        'How many degrees does a triangle have total for every corner?',
-                    imageSrc:
-                        'https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000',
-                  ),
-                  Question(
-                    category: 'Math',
-                    questionContent: 'How do you define a quantum singularity?',
-                    imageSrc:
-                        'https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000',
-                  ),
-                  Question(
-                    category: 'Math',
-                    questionContent:
-                        'What is the best angle to throw a ball for it to have the largest travel distance?',
-                    imageSrc:
-                        'https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000',
-                  ),
-                  Question(
-                    category: 'Math',
-                    questionContent: 'What is the meaning of life?',
-                    imageSrc:
-                        'https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg?w=2000',
-                  ),
+                  ...questions
                 ],
               ),
             ),
